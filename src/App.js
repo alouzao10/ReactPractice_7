e2e01a01a5dd9744c4ae0e6b5bfb3dd086369170
alouzao10 <alouzao10@gmail.com>
2020-04-15 14:25:02 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Hello from './Components/Hello';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello Mundo...</h1>
+        <hr />
+        <h2>
+          {this.props.txt} + {this.props.num}
+        </h2>
+        <Hello />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Hello.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+  render() {
+    return (
+      <div>
+        <h2 className='greeting'>Hello</h2>
+      </div>
+    );
+  }
+}
+
+export default Hello;
